8e27f08f009cd550874c6e917e4137949c205db7
danlek <gabriel.keldon@gmail.com>
2010-11-26 13:52:46 +0100

    Version 0.2 Lagt till några metoder för att hantera meddelanden och fixat ett par variabler.
    Signed-off-by: danlek <gabriel.keldon@gmail.com>

--- Laboration 3/src/arrayHanteraren.java
+++ Laboration 3/src/arrayHanteraren.java
@@ -2,18 +2,30 @@ import java.util.Random;
 public class arrayHanteraren 
 {
 	private int[] slumpadeTal = new int[100];
+	public boolean arrayPopulated = false;
+	public boolean arraySorted = false;
 	private int dice()
 	{
 		Random dice = new Random();
 		return dice.nextInt(900);
 	}
+	private int get(int index)
+	{
+		return slumpadeTal[index];
+	}
+	private void set(int index, int value)
+	{
+		slumpadeTal[index] = value;
+	}
 	public void populate()
 	{
-		//int[] slumpadeTal = new int[100];
 		for(int i = 0; i < 100; i++)
 		{
-			slumpadeTal[i] = dice();
+			//slumpadeTal[i] = dice();
+			set(i, dice());
 		}
+		arrayPopulated = true;
+		arraySorted = false;
 	}
 	public void display()
 	{
@@ -22,12 +34,12 @@ public class arrayHanteraren
 		{
 			for(int j = counter; j < (counter + 10); j++)
 			{
-				System.out.print(slumpadeTal[j] + " ");
+				//System.out.print(slumpadeTal[j] + " ");
+				System.out.print(get(j) + " ");
 			}
 			System.out.println("");//radbyte
 			counter = counter + 10;
-		}
-		
+		}	
 	}
 	public void sort()
 	{
@@ -41,12 +53,16 @@ public class arrayHanteraren
 			swaped = false;
 			for(int k = 0; k < 99; k++)
 			{
-				tal1 = slumpadeTal[k];
-				tal2 = slumpadeTal[k + 1];
+				//tal1 = slumpadeTal[k];
+				//tal2 = slumpadeTal[k + 1];
+				tal1 = get(k);
+				tal2 = get(k + 1);
 				if(tal1 > tal2)
 				{
-					slumpadeTal[k + 1] = tal1;
-					slumpadeTal[k] = tal2;
+					//slumpadeTal[k + 1] = tal1;
+					//slumpadeTal[k] = tal2;
+					set(k + 1, tal1);
+					set(k, tal2);
 					swaped = true;
 				}
 				else if(k == 98 && swaped == false)
@@ -55,6 +71,23 @@ public class arrayHanteraren
 				}
 			}
 		}
+		arraySorted = true;
 	}
-	
+	public void msg(int Typ)
+	{
+		String msg;
+		switch(Typ)
+		{
+		case 1: System.out.println("Du måste skapa en array med slumpade siffor innan du kan ordna det. Välj alternativ 1 först");
+				break;
+		case 2: System.out.println("Du måste ordna talföljden innan du kan söka efter specifika tal eller få ut statistik om talföljden." +
+				"\nVälj alternativ 2 först");
+				break;
+		case 3: System.out.println("1) Skapa en array med slumpade siffor! 2) Ordna array'n! 3) Få statistik om siffrorna i array'n!" +
+				" 4) Sök efter ett specifikt tal i array'n");
+				break;
+		}
+		
+		
+	}
 }
--- Laboration 3/src/lab3.java
+++ Laboration 3/src/lab3.java
@@ -1,32 +1,70 @@
 import java.util.Scanner;
 public class lab3 
 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) 
 	{
 		arrayHanteraren slump = new arrayHanteraren();
-		System.out.println("1 för att skapa en array med slumpade siffor! 2 för att ordna arrayen!");
 		Scanner kommandon = new Scanner(System.in);
-		int kommand = kommandon.nextInt();
-		
-		
-		
-			if(kommand == 1)
-			{
-				slump.populate();
-				slump.display();
-			}
-			else if(kommand == 2)
-			{
-				slump.populate();
-				slump.display();
-				slump.sort();
-				slump.display();
-			}
-		
-	}
+		int kommand = 1;
 
+		System.out.println("Välkommen till Slumpatron version 0.1! \nVälj bland följande alternativ!");
+		while(kommand != 0)
+		{
+			slump.msg(3);
+			kommand = kommandon.nextInt();
+			if(slump.arraySorted == true)
+			{
+				//Om sant så kan man köra alla kommandon.
+				if(kommand == 1)
+				{
+					slump.populate();
+					slump.display();
+				}
+				else if(kommand == 2)
+				{
+					slump.sort();
+					slump.display();
+				}
+				else if(kommand == 3)
+				{
+					//TODO skapa metod som hanterar sån här bajs
+				}
+				else if(kommand == 4)
+				{
+					//TODO skapa metod som hanterar sån här bajs
+				}
+			}
+			else if(slump.arraySorted == false && slump.arrayPopulated == true)
+			{
+				//Om detta är sant så kan man bara köra kommando 1 och 2.
+				if(kommand == 1)
+				{
+					slump.populate();
+					slump.display();
+				}
+				else if(kommand == 2)
+				{
+					slump.sort();
+					slump.display();
+				}
+				else if(kommand > 2)
+				{
+					slump.msg(2);
+				}
+			}
+			else if(slump.arraySorted == false && slump.arrayPopulated == false)
+			{
+				//Om detta är sant så kan man bara köra första kommandot.
+				if(kommand == 1)
+				{
+					slump.populate();
+					slump.display();
+				}
+				else if(kommand > 1)
+				{
+					slump.msg(1);
+				}
+			}
+		}
+	}
 }
